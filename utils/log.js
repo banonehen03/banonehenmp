b5dbd50d5261f2b79982ebfabd5d12e320a65895
Khuat Anh Viet <89727348+banonehen03@users.noreply.github.com>
2022-02-24 22:32:50 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.yellow('[ ❕ ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.red('[ ❕ ] » ') + data);
 			break;
 		default:
 			console.log(chalk.magenta(`${option} » `) + data);
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[D-JUKIE ] » ') + data);
+			console.log(chalk.yellow('[ Viet ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[D-JUKIE ] » ') + data);
+			console.log(chalk.red('[ Viet ] » ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[D-JUKIE ] » `) + data);
+			console.log(chalk.green(`[ Viet ] » `) + data);
 			break;
 	}
 }
